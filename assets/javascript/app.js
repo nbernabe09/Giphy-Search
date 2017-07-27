6c10027539839a8f5276ce957145e52cfbfeb705
Nathan Bernabe <nbernabe09@aol.com>
2017-07-27 16:23:11 -0700

    Added AJAX and GIPHY API functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,48 @@
 // Variables
-var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+var topics = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
 
 function displayGiphy() {
-	var animal = $(this).attr("data-name");
-	var queryURL = "";
+	var topic = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=355651bd3b534a1bbfddb31e12a67a41&q=" + topic + "&limit=10&offset=0&rating=PG-13&lang=en";
+	// var apiKey = "355651bd3b534a1bbfddb31e12a67a41";
 
 	$.ajax({
 		url: queryURL,
 		method: "GET"
 	}).done(function(response) {
-
+		console.log(response);
+		$("#giphy-view").html("");
+		for (var i = 0; i < response.data.length; i++) {
+			newDiv = $("<div>");
+			newDiv.append("<p>Rating: " + response.data[i].rating + "</p>");
+			newDiv.append("<div><img src='" + response.data[i].images.fixed_height_still.url + "'></div>");
+			$("#giphy-view").append(newDiv);
+		}
 	});
 }
 
 function renderButtons() {
 	$("#btn-view").empty();
-	for (var i = 0; i < animals.length; i++) {
+	for (var i = 0; i < topics.length; i++) {
 		var a = $("<button>");
-		a.addClass("animal btn btn-info");
-		a.attr("data-name", animals[i]);
-		a.text(animals[i]);
+		a.addClass("topic btn btn-info");
+		a.attr("data-name", topics[i]);
+		a.text(topics[i]);
 		$("#btn-view").append(a);
 	}
 }
 $("#add-giphy").on("click", function(event) {
 	event.preventDefault();
-	var animal = $("#giphy-input").val().trim();
-	if (animal === "") {
+	var topic = $("#giphy-input").val().trim();
+	if (topic === "") {
 		return
 	}
-	if (!animals.includes(animal)) {
-		animals.push(animal);
+	if (!topics.includes(topic)) {
+		topics.push(topic);
 	}
 	renderButtons();
 });
 
-$(document).on("click", ".animal", displayGiphy);
+$(document).on("click", ".topic", displayGiphy);
 
 renderButtons();
